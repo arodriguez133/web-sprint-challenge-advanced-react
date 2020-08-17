5771bd02b0b7fafe7b4df0d2fb369101c0542a97
Adan Rodriguez <adanrodriguez@Kourtneys-MBP.hitronhub.home>
2020-08-16 20:52:59 -0500

    added custom hooks for controlling the checkout form.

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,23 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
+test("form header renders", () => {
+    const {getByText}= render(<CheckoutForm/>)
+    const header = getByText(/checkout form/i)
+    expect(header).toBeInTheDocument()
+    
+});
 
-test("form header renders", () => {});
+test("form shows success message on submit with form details", () => {
+    const { getByLabelText, getByTestId, getByText } = render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    fireEvent.change(getByLabelText(/first name/i), { target: { value: "Adan" } })
+    fireEvent.change(getByLabelText(/last name/i), { target: { value: "Rodriguez" } })
+    fireEvent.change(getByLabelText(/address/i), { target: { value: "1202 Panther Ave" } })
+    fireEvent.change(getByLabelText(/city/i), { target: { value: "Charlotte" } })
+    fireEvent.change(getByLabelText(/state/i), { target: { value: "NC" } })
+    fireEvent.change(getByLabelText(/zip/i), { target: { value: "61241" } })
+    
+});
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,22 @@
 // write your custom hook here to control your checkout form
+import React, { useState } from 'react';
+    const useForm = ( initialValue ) => {
+        const [ showSuccessMessage, setShowSuccessMessage ] = useState( false );
+        const [ values, setValues ] = useState( initialValue );
+        const [ order, setOrder ] = useState ({});
+
+        const handleChanges = (e) => {
+            setValues({ ...values, [e.target.name]: e.target.value });
+        }
+
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            setShowSuccessMessage(true);
+            setOrder(values)
+            setValues(initialValue)
+            console.log('Success')
+         }
+         return [showSuccessMessage, values, setValues, handleChanges, handleSubmit, order]
+    }
+
+    export default useForm;
